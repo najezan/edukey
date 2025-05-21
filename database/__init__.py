a630a557d8b9c2fbdcc5f66c03ed1e5ff2bf7f07
najezan <fakhrezan1@gmail.com>
2025-05-21 16:04:04 +0700

    fix(ui): Fix darkmode bug ui

--- database/__init__.py
+++ database/__init__.py
@@ -3,6 +3,5 @@ Database package initialization.
 """
 
 from database.db_manager import DatabaseManager
-from .mysql_db_manager import MySQLDatabaseManager
 
-__all__ = ['DatabaseManager', 'MySQLDatabaseManager']
+__all__ = ['DatabaseManager']
